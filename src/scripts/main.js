84dee16ffd43ec9bb59b433e637c7218539c9ad9
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-05-29 14:57:15 +0545

    Load Atelier treatment across subpages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ import { onReady } from './utils/dom.js';
 import { injectGlobalStyles } from './features/global-styles.js';
 import { normalizeArticleLayout } from './features/article-layout.js';
 import { initActiveNavigation } from './features/navigation.js';
+import { initAtelierPages } from './features/atelier-pages.js';
 import { initContactForm } from './features/contact-form.js';
 import { initFilters } from './features/filters.js';
 import { initMobileMenu } from './features/mobile-menu.js';
@@ -9,13 +10,12 @@ import { initMotionEnhancements } from './features/motion.js';
 import { useProjectDetailImages } from './features/project-images.js';
 import { initResumeDownload } from './features/resume.js';
 import { ensureSiteFooter } from './features/site-footer.js';
-import { initTheme } from './features/theme.js';
 
 onReady(() => {
   injectGlobalStyles();
   normalizeArticleLayout();
+  initAtelierPages();
   useProjectDetailImages();
-  initTheme();
   initMobileMenu();
   initActiveNavigation();
   initFilters();
