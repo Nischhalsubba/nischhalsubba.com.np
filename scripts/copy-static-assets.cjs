c520079d493aee2cdf41cffdc199bb581c7f8978
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-06-26 07:26:58 +0545

    Use v39 stylesheet during build copy

--- scripts/copy-static-assets.cjs
+++ scripts/copy-static-assets.cjs
@@ -4,7 +4,7 @@ const { EARLY_THEME_BOOTSTRAP } = require('./early-theme-bootstrap.cjs');
 
 const rootDir = path.resolve(__dirname, '..');
 const distDir = path.join(rootDir, 'dist');
-const styleHref = '/style.css?v=38.0';
+const styleHref = '/style.css?v=39.0';
 const scriptSrc = '/script.js?v=32.0';
 
 function copyDirectory(source, target) {
